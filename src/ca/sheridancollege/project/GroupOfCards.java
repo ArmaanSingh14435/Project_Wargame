5163618010afbf34926cf4253e456fae5f4599c3
arshdeep-singh6 <sin14582@sheridancollege.ca>
2024-06-09 16:35:48 -0400

    repository created

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -9,13 +9,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
- * once. The group of cards has a maximum size attribute which is flexible for reuse.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
+
 public class GroupOfCards {
 
     private final List<PlayingCard> cards; // The concrete subclass
